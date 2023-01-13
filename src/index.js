ea4b90c60ef3e6997d0c5eac3e84fe79e4b5aa7d
Janira Crispin <janiracrispin@gmail.com>
2023-01-13 16:47:13 -0500

    create initial class setup

---
+++ src/index.js
@@ -0,0 +1,7 @@
+import Finanzas from "./scripts/finanzas"; 
+// import "./styles/main.scss";
+
+document.addEventListener("DOMContentLoaded", () => {
+    const testing = new Finanzas("Love YOU")
+    testing.start()
+})
---
+++ src/scripts/finanzas.js
@@ -0,0 +1,11 @@
+class Finanzas {
+    constructor(startingMessage){
+        this.startingMessage = startingMessage;
+    }
+
+    start(){
+        // alert(this.startingMessage);
+    }
+}
+
+export default Finanzas;
